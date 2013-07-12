98fa639b847e4dcc14863ec5714f043b2a721754
Devin Weaver <suki@tritarget.org>
2013-07-12 11:55:12 -0400

    Adds a persistent Sandbox for evaluating code
    Uses a custom sandbox object to attach a context for any code being
    executed. This sandbox will store a JS context between eval executions.
    It prevent global name space pollution.
    
    Adds ability to send non JavaScript commands such as `/reset` (Only
    command supported so far).
    
    Reset command will create a new sandbox environment and garbage collect
    the last one.

--- src/logger.js
+++ src/logger.js
@@ -4,12 +4,51 @@ function debug(message, method) {
   Ti.API.debug("[Logger" + (method != null ? "." + method : "") + "] " + message);
 }
 
+var Sandbox = function(logger) {
+  // Normally saving a reference to logger whould no be a problem. However we
+  // use "this" as the context to execute the eval. So we want to hide logger
+  // and only expose the log function. Because of this we use a closure to
+  // hide the logger object but still offer a log function.
+  this.log   = function() { return logger.log.apply(logger, arguments); };
+  this.reset = function() { return logger.reset.apply(logger, arguments); };
+};
+
+Sandbox.exec = function(code, sandbox) {
+  /* jshint evil:true, boss:true */
+  var result;
+  if (result = code.match(/^\s*\/([A-za-z]+)(?:\s+(.+))?$/)) {
+    return Sandbox.command(sandbox, result[1].toLowerCase(), result[2]);
+  }
+  else {
+    return eval("(function(exports){" + code + "}).call(sandbox,sandbox)");
+  }
+};
+
+Sandbox.command = function(/* sandbox, command, options... */) {
+  var
+    sandbox = arguments[0],
+    command = arguments[1],
+    options = 3 <= arguments.length ? [].slice.call(arguments, 2) : [];
+  switch (command) {
+    case "reset":
+      sandbox.reset();
+      break;
+    default:
+      throw "Unknown command: " + command;
+  }
+};
+
 var Logger = function(host,port) {
   this.host = host;
   this.port = port;
   this.socket = null;
   this.connected = false;
   this._msgs = []; // just to queued up the messages
+  this.reset();
+};
+
+Logger.prototype.reset = function() {
+  this.sandbox = new Sandbox(this);
 };
 
 Logger.prototype.connect = function() {
@@ -21,7 +60,7 @@ Logger.prototype.connect = function() {
   });
   this.socket.addEventListener('read', function(msg) {
     try {
-      eval('(function(){' + msg.data + '})()' );
+      self.log(Sandbox.exec(msg.data,self.sandbox));
     } catch(ex) {
       self.log(ex);
       debug(ex, "connect");
